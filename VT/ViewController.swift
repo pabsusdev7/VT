a620d82aa4dd777f30aaf1ef699bc9d44e81c8ab
pabsusdev7 <pabsusdrumer@gmail.com>
2020-06-24 07:16:07 -0500

    Initial Commit

---
+++ VT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VT
+//
+//  Created by Pablo Albuja on 6/24/20.
+//  Copyright © 2020 Ingenuity Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
